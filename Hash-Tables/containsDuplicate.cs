17187b3d74bdd2a2be4f45f3147fec8fe493dffe
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-07-16 20:03:10 +0100

    Add solution to the problem of finding duplicates using hash set

---
+++ Hash-Tables/containsDuplicate.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hset = new HashSet<int>();
+        
+        foreach(int num in nums){
+            if (hset.Contains(num)){
+                return true;
+            }             
+            hset.Add(num);
+        }
+        return false;
+    }
+}
