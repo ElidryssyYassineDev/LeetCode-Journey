269423ec94cf976b7216e2d14257181d432b0047
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-07-19 23:02:47 +0100

    Add solution to the problem of single number

---
+++ Hash-Tables/singleNumber.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int unique = 0;
+        foreach(int num in nums){
+            unique ^= num;
+        }
+        return unique;
+    }
+}
