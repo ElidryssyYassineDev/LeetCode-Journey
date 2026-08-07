8e26731c57c788836bb7378a3ac4ed49fa9fb5e7
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-08-07 22:10:28 +0100

    Add solution to the problem of Fibonacci sequence (memoization)

--- Recursion I/FibonacciNumber.cs
+++ Recursion I/FibonacciNumber.cs
@@ -1,9 +1,21 @@
 public class Solution {
+    
+    Dictionary<int, int> cache = new Dictionary<int, int>();
+    
     public int Fib(int n) {
+        if (cache.ContainsKey(n)){
+            return cache[n];
+        }
+        int result;
+        
         if (n < 2){
             return n;
+        }else{
+            result = Fib(n - 1) + Fib(n - 2);
         }
+        cache.Add(n,result);
+        return result;
         
-        return Fib(n - 1) + Fib(n - 2);
+        
     }
 }
