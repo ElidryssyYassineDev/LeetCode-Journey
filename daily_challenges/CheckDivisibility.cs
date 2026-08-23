9566855328e676b272712f325877ca222cfda212
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-08-23 23:38:12 +0100

    Add solutiion to the problem of checking divisibility of sum and product of digits

---
+++ daily_challenges/CheckDivisibility.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CheckDivisibility(int n) {
+        int product = 1;
+        int sum = 0;
+        int temp = n;
+
+        while (temp > 0){
+            sum += (temp % 10);
+            product *= (temp % 10);
+            temp /= 10;
+        }
+
+        return n % (sum + product) == 0;
+    }
+}
