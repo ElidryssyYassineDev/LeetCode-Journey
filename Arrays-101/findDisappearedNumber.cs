467da658eaf89c2c2fd4c64bee7c13c1118b32c0
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-09-04 23:55:03 +0100

    Add solution to the problem of finding disappered numbers in an array

--- Arrays-101/findDisappearedNumber.cs
+++ Arrays-101/findDisappearedNumber.cs
@@ -1,16 +1,15 @@
 public class Solution {
     public IList<int> FindDisappearedNumbers(int[] nums) {
-        HashSet<int> numbers = new HashSet<int>(nums);//use a hash set to store the numbers in the input array for O(1) lookups
-        foreach(int num in nums){
+        IList<int> result = new List<int>();
+        HashSet<int> numbers = new HashSet<int>();
+        foreach (int num in nums) {
             numbers.Add(num);
-        } 
-        
-        IList<int> disappearedNumbers = new List<int>();
-        for(int i=1;i<=nums.Length;i++){
-            if(!numbers.Contains(i)){
-                disappearedNumbers.Add(i);//if the number is not in the hash set, it means it is missing from the input array, so we add it to the result list
+        }
+        for (int i = 1; i <= nums.Length; ++i){
+            if (!numbers.Contains(i)){
+                result.Add(i);
             }
         }
-        return disappearedNumbers;
+        return result;
     }
 }
